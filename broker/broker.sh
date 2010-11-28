6e0fc9f24f995d31e8937762ceaabe016b7a703b
Will Brown <will.h.brown@gmail.com>
2010-11-28 17:36:17 -0500

    Updated broker and run to make running on other brokers easier.

--- broker/broker.sh
+++ broker/broker.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-ncat --broker --listen 7000 -o ~/logs/`date +"%y%m%d-%H%M%S"`.txt &
+if [ "$1" == '' ]; then
+    LOGFILE=~/logs/`date +"%y%m%d-%H%M%S"`.txt
+else
+    LOGFILE=$1
+fi
+
+echo Logging to $LOGFILE
+ncat --broker --listen 7000 -o $LOGFILE &
 ncat --broker --listen 7001 &

487648f1466168c8e5a217965e2e739e95b8e897
Will Brown <will.h.brown@gmail.com>
2011-03-07 12:09:30 -0500

    Removed a test class that was in the root directory.

--- NavTest.java
+++
@@ -1,9 +0,0 @@
-import org.haldean.chopper.nav;
-
-public class NavTest {
-	
-	public static void main (String args[]) {
-		NavList myList = NavList.fromString( "DEST!-0.001559631878376859!23.265086094158125!-19.352605170513353!1.0!10.0" );
-	}
-}
-
